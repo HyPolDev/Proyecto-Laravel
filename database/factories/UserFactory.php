8d42d1f18403bc657636af74024ec0cf8887c0a3
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-04-24 08:46:57 +0200

    Feat: Created admin & superadmin users from DatabaseSeeder.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,26 +29,7 @@ class UserFactory extends Factory
 
         ];
     }
-    // public function superAdmin()
-    // {
-    //     return [
-    //         'userName' => "ramiro",
-    //         'email' => "ramiro@gmail.com",
-    //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-    //         'roleName' => 3,
-    //         'remember_token' => Str::random(10),
-    //     ];
-    // }
-    // public function admin()
-    // {
-    //     return [
-    //         'userName' => "marina",
-    //         'email' => "marina@gmail.com",
-    //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-    //         'roleName' => 2,
-    //         'remember_token' => Str::random(10),
-    //     ];
-    // }
+
 
     /**
      * Indicate that the model's email address should be unverified.
