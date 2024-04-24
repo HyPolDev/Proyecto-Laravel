dc6af1847c04b647a6fef8ab9816a1e1f3c54e3b
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-04-24 09:42:31 +0200

    Check branch

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,7 +32,7 @@ class UserFactory extends Factory
 
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the model's email address should be unverified.////
      *
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
