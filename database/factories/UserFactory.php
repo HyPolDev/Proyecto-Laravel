ecff53780f7c1bb19b9eeb43b20bbfdb939fad84
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-04-24 10:25:02 +0200

    fixed seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,41 +17,31 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
 
-    public function definition()
-    {
+     public function definition()
+     {
+         static $count = 0;
+         $names = ['Marina', 'Ramiro'];
+         $roleNames = ['Marina' => 2, 'Ramiro' => 3];
 
-        static $count = 0;
-        $names = ['Marina', 'Ramiro'];
-        $roleNames = ['Marina' => 2, 'Ramiro' => 3];
-
-        if ($count < count($names)) {
-            $name = $names[$count];
-            $count++;
-            return [
-                'userName' => $name,
-                'email' => strtolower($name) . '@gmail.com',
-                'roleName' => $roleNames[$name],
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ];
-        } else {
-            return [
-                'userName' => fake()->name(),
-                'email' => fake()->unique()->safeEmail(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ];
-        }
-        return [
-            'userName' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            // 'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-
-
-        ];
-    }
+         if ($count < count($names)) {
+             $name = $names[$count];
+             $count++;
+             return [
+                 'userName' => $name,
+                 'email' => strtolower($name) . $count . '@gmail.com', // Añade el contador al email para hacerlo único
+                 'roleName' => $roleNames[$name],
+                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                 'remember_token' => Str::random(10),
+             ];
+         } else {
+             return [
+                 'userName' => fake()->name(),
+                 'email' => fake()->unique()->safeEmail(),
+                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                 'remember_token' => Str::random(10),
+             ];
+         }
+     }
 
 
     /**
